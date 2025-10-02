997eb26c75aae6bd8cfe2e303204e853ec1a48fd
1709abhishek <1709abhishek@gmail.com>
2025-10-02 15:00:28 -0500

    additional changes

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,28 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: [
+              "default-src 'self'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com https://*.gstatic.com",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
+              "img-src 'self' data: https: blob:",
+              "font-src 'self' data: https://fonts.gstatic.com",
+              "connect-src 'self' https://*.googleapis.com https://*.gstatic.com https://*.google.com",
+              "frame-src https://*.googleapis.com https://*.google.com",
+              "worker-src 'self' blob:",
+            ].join('; '),
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
